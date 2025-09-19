56be4cc962cf7ec4fd3ec10cdc860a0f8eb568cb
hadia-noor16 <68270205+hadia-noor16@users.noreply.github.com>
2025-09-18 23:08:24 -0600

    cicd pipeline execution

--- main.tf
+++ main.tf
@@ -256,3 +256,238 @@ viewer_certificate {
     cloudfront_default_certificate = true
   }
 }
+
+# s3 srtifacts bucket for ci/cd pipeline
+resource "aws_s3_bucket" "artifacts" {
+  bucket = "hn-artifacts123"
+}
+resource "aws_s3_bucket_ownership_controls" "owner" {
+  bucket = aws_s3_bucket.artifacts.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+resource "aws_s3_bucket_public_access_block" "acl" {
+  bucket = aws_s3_bucket.artifacts.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+resource "aws_s3_bucket_policy" "s3_policy" {
+  depends_on = [aws_s3_bucket_public_access_block.acl]
+  bucket = aws_s3_bucket.artifacts.id
+ 
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : "*",
+        "Action" : "s3:GetObject",
+        "Resource" : ["${aws_s3_bucket.artifacts.arn}/*"]
+      }
+    ]
+  })
+}
+
+
+# Service role for CodeBuild and assume role
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["codebuild.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "example" {
+  name               = "example"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "example" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["*"]
+  }
+
+
+  statement {
+    effect  = "Allow"
+    actions = ["s3:*"]
+    resources = [
+      "${aws_s3_bucket.artifacts.arn}/*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "codeconnections:GetConnectionToken",
+      "codeconnections:GetConnection"
+    ]
+    resources = ["arn:aws:codeconnections:us-east-1:957196010799:connection/05fcb4eb-3c4f-42d6-910a-b6474474cdd6"]
+  }
+}
+
+resource "aws_iam_role_policy" "example" {
+  role   = aws_iam_role.example.name
+  policy = data.aws_iam_policy_document.example.json
+}
+
+
+
+# CI/CD Build phase with github repo as source, defining where build project artifacts go to in s3 bucket, and the specs of server to run this task
+
+resource "aws_codebuild_project" "project" {
+  name          = "my-codebuild-project"
+  description   = "Build project for my website"
+  service_role  = aws_iam_role.example.arn
+  source {
+    type      = "GITHUB"
+    location  = "https://github.com/hadia-noor16/CICD/website"
+    buildspec = "buildspec.yml"  
+  }
+  artifacts {
+    type = "S3"
+    location = aws_s3_bucket.artifacts.id
+  }
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+ }
+}
+
+
+
+# Code pipeline with source, build, and deploy stage (to s3 dev bucket)
+resource "aws_codepipeline" "pipeline" {
+  name     = "my-pipeline"
+  role_arn = aws_iam_role.codepipelinerole.arn
+artifact_store {
+    location = aws_s3_bucket.artifacts.id
+    type     = "S3"
+  }
+  stage {
+    name = "Source"
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeStarSourceConnection"
+      version          = "1"
+      output_artifacts = ["source_output"]
+      configuration = {
+        ConnectionArn = var.github_connector  # create variable and save the connector name in .tfvars file
+        FullRepositoryId = "hadia-noor16/CICD"  # Update with your GitHub repo name
+        BranchName = "main"  # Specify the branch
+      }
+    }
+  }
+  stage {
+    name = "Build"
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      version          = "1"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+      configuration = {
+        ProjectName = aws_codebuild_project.project.name
+      }
+    }
+  }
+  stage {
+    name = "Deploy"
+    action {
+      name             = "Deploy"
+      category         = "Deploy"
+      owner            = "AWS"
+      provider         = "S3"
+      version          = "1"
+      input_artifacts  = ["build_output"]
+      configuration = {
+        BucketName = var.dev_bucket  # my dev bucket
+        Extract    = "true"
+      }
+    }
+  }
+}
+
+# CodePipeline role
+
+data "aws_iam_policy_document" "assume_role_pipeline" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["codepipeline.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role"  "codepipelinerole" {
+  name = "codepipelinerole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_pipeline.json
+}
+
+data "aws_iam_policy_document" "codepipeline_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetBucketVersioning",
+      "s3:PutObjectAcl",
+      "s3:PutObject",
+    ]
+
+    resources = [
+      aws_s3_bucket.artifacts.arn,
+      "${aws_s3_bucket.artifacts.arn}/*"
+    ]
+  }
+
+  statement {
+    effect    = "Allow"
+    actions   = ["codestar-connections:UseConnection"]
+    resources = [var.github_connector]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "codebuild:BatchGetBuilds",
+      "codebuild:StartBuild",
+    ]
+
+    resources = ["*"]
+  }
+}
+resource "aws_iam_role_policy" "codepipelinerole" {
+  role   = aws_iam_role.codepipelinerole.name
+  policy = data.aws_iam_policy_document.codepipeline_policy.json
+}
